684e32d3f0d60856aae92124a39cf556a4548200
Banderas13 <raynier97@hotmial.com>
2025-04-22 21:05:57 +0200

    feat: add headerLeft option to index screen for improved navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack>
-    <Stack.Screen name="index" options={{ title: "Sticker Smash"}}/>
+    <Stack.Screen name="index" options={{ title: "Sticker Smash", headerLeft: () => <></>}}/>
     <Stack.Screen name="about" options={{ title: "About"}}/>
     <Stack.Screen name="+not-found" options={{ headerShown: false }}/>
   </Stack>;
